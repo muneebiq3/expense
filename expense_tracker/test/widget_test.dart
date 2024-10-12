b80d93544ef00750d76b9b44d313b848ce56341d
muneebiq3 <muneebiq526@gmail.com>
2024-10-12 18:27:29 +0500

    first test run on p8p emulator

--- expense_tracker/test/widget_test.dart
+++ expense_tracker/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:expense_tracker/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(BudgetApp());
+    await tester.pumpWidget(const BudgetApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
